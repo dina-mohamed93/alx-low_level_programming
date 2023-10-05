bb9143ae48c386bc8c7745e5882ed604813973f6
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-05 11:53:57 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point 
+ * main - Entry point 
  *
  * Description: A C program that prints with put function 
  *
- * Return: Always 0 (Success)*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
