6d9d10be5f267b7773d9c01f8d8956cfd3c16466
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-07 09:29:56 -0700

    My first commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
+ * Description: print all aplhabet letters
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char low;
-
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
-
+	char ch = 'a';
+	while ( ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
 	return (0);
 }
