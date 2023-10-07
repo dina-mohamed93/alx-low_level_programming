a820e2d952aabff40a16eb36721f891f36fe44fa
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-07 10:34:26 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print alphabet in lower case 
+ *
+ * Return: 0
+ */
+int main(void)
+{
+        char ch = 'a';
+
+        while (ch <= 'z')
+        {
+		if (ch =='q' || ch=='e')
+			ch++;
+                putchar(ch);
+		ch++;
+        }
+        putchar('\n');
+        return (0);
+}
