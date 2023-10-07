beaf3ab2aef9e43e20338218dcb0bcd28a7a862a
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-07 15:17:45 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print the lowwercase alphabet in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int digit = 0;
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digital++;
+	}
+	putchar('\n');
+	return (0);
+}
