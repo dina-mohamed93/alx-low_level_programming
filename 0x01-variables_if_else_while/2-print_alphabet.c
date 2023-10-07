9ab285aeca17371dae57365780e9901f63c8f985
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-07 09:01:19 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int()
+{
+	char = 'a';
+	while (ch <= 'z')
+	{
+		putchar('ch');
+		ch++;
+
+	}
+	putchar('\n');
+
+	return (0);
+
+
+}
