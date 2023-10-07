13b23b58e1faa9b8a78f0e0dbc01017ff2d48287
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-07 14:30:08 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print the lowwercase alphabet in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch => 'a')
+	{
+		putchar('ch');
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
