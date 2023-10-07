1a51fb76c462fab32c1e336011cfd5abc0294698
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-07 15:27:38 -0700

    My first commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,15 +8,16 @@
  */
 int main(void)
 {
-	int digit = 48;
+        int digit = 48;
 
-	while (digit <= 102)
-	{
-		putchar(digit);
-		if (digit == 57)
-			digit += 39;
-		digit++;
-	}
+        while (digit <= 102)
+        {
+                putchar(digit);
+                if (digit == 57)
+                        digit += 39;
+                digit++;
+        }
         putchar('\n');
         return (0);
 }
+
