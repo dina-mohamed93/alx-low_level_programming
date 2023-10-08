cadc8246e82aaf35c3c5e5c5c46081c37e50c0a4
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-08 02:47:02 -0700

    My first commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-	int i, j;
-	for (i = 0;i <=9; i++)
-	{
-		for (j = 0 ;j <= 9;j++)
-			if(i == j)
-				i++;
-		printf("%i%i,", ij);
-	return (0);
-	}
+        int i, j;
+        for (i = 0;i <=9; i++)
+        {
+                for (j = 0 ;j <= 9;j++)
+                        if(i == j)
+                                i++;
+                printf("%i%i,", ij);
+        return (0);
+        }
 }
