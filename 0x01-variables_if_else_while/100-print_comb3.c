26a53c565dee72a27a20cb82a4c2f9a214455be0
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-08 02:41:05 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print lowwer and upper alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+	for (i = 0;i <=9; i++)
+	{
+		for (j = 0 ;j <= 9;j++)
+			if(i == j)
+				i++;
+		printf("%i%i,", ij);
+	return (0);
+	}
+}
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,9 @@ int main(void)
 	int digit = 48;
 
 	while (digit <= 102)
-        {
+	{
 		putchar(digit);
-                if (digit == 57)
+		if (digit == 57)
 			digit += 39;
 		digit++;
         }
