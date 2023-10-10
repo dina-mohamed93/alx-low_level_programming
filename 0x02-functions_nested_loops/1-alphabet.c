1b2ee542650a73abae2076026b42cd1e01cb6488
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-10 15:22:07 -0700

    My first commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_alphabet - utilizes on the _putchar function to print 
- *
- * 
+ * print_alphabet - Entry point
+ * Return:0
  */
 
 void print_alphabet(void)
 {
 	int ch;
 
-	for(ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
 }
