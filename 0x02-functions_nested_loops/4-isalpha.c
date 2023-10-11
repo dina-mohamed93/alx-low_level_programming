48fa5b224155ba3910a10ad2ca5b5f338b7c452e
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-11 12:33:18 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - check for lower case letter
+ * @c : character to check
+ * Return:0 or 1
+ */
+
+int _isalpha(int c)
+{
+	if (c == 67 || c == 99)
+		return (1);
+	return (0);
+}
