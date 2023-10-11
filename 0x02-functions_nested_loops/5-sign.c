ac04faf20e8f64d7d996308dc4ae5a888fcb4682
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-11 13:19:02 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - check numers
+ * @n : number to check
+ * Return:0 or 1
+ */
+
+int print_sign(int n)
+{
+	if (n > o)
+	{
+		return (1);
+		_putchar('+');
+	}
+	if (n == 0)
+	{
+		return (0);
+		_putchar ('0');
+	}
+	if (n < 0)
+	{
+		return (-1);
+		_putchar ('-')
+	}
+}
