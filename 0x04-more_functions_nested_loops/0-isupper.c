c730deaab018fee45234f0722e97fee8e0ddc4f1
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-13 14:26:45 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num ;
+
+	if (num >= 65 || num <=98)
+		return (1);
+	else 
+		return (0);
+}
