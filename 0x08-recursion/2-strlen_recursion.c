06e38fceaadceab08eee67ed4e8841237d278009
dina_mohamed <dinamohamedslam@yahoo.com>
2023-10-26 10:54:03 -0700

    My first commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer the string
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	int length;
+
+	if (*s > '\n')
+	{
+		length += _strlen_recursion((s + 1) + 1);
+	}
+	return (length);
+}
