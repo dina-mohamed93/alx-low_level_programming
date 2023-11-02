56701fe269032730e9f394f50c070467474df67a
dina_mohamed <dinamohamedslam@yahoo.com>
2023-11-02 14:33:39 -0700

    My commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,29 +10,42 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int i, j, s1_length, s2_length;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
-		;
-	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
-		;
-	str = malloc(s1_length + n + 1);
-	if (str == NULL)
+
+	while (s1[size1] != '\0')
 	{
-		return (NULL);
+		size1++;
 	}
-	for (i = 0; s1[i] !=  '\0'; i++)
-		str[i] = s1[i];
-	for (j = 0; j < n; j++)
+
+	while (s2[size2] != '\0')
 	{
-		str[i] = s2[j];
-		i++;
+		size2++;
 	}
-	str[i] = '\0';
-	return (str);
+
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (0);
+
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+return (p);
 }
