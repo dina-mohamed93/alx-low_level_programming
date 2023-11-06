92d65ad12af6ddb90fde4489f86b865f1a25119e
dina_mohamed <dinamohamedslam@yahoo.com>
2023-11-06 14:57:20 -0800

    My commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,22 +11,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name != NULL)
-		{
-			printf("Name: %s\n", d->name);
-		}
-		else
-		{
-			printf("Name: (nil)\n");
-		}
+		printf("Name: %s\n", d->named ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
-		if (d->owner)
-		{
-			printf("Owner: %s\n", d->owner);
-		}
-		else
-		{
-			printf("Owner: (nil)\n");
-		}
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
