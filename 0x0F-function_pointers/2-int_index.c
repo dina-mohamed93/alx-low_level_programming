8ccecf7e20dc3193306e377545f62ec00901ea05
dina_mohamed <dinamohamedslam@yahoo.com>
2023-11-08 12:19:36 -0800

    My commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - maps an array through a func pointer
+ * @array : the int array
+ * @size: the array size
+ * @action: function pointer
+ *
+ * return: void
+ */
+
+int int_index(int *array, int size, int (*cmp)(int));
+{
+	int *end = array + size -1;
+
+	if (array && size && action)
+		while (array <= end)
+			action(*array++);
+}
