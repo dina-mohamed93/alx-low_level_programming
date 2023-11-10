0e7e8dab005961b4a8194bf88aec776b8edcf531
dina_mohamed <dinamohamedslam@yahoo.com>
2023-11-10 11:08:41 -0800

    My commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,6 +7,7 @@
  * print_strings - prints all strings with a separator
  * @separator: separe strings
  * @n: int, number of undefined arguments
+ * Return: void
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
@@ -14,6 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list list;
 	unsigned int i;
 	char *c;
+
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
